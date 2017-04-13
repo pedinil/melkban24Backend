f0c22b37a5a36a0002f4ec268517ed6704aac0c4
Mehdi <mahdilacki@gmail.com>
2017-04-13 15:28:55 +0430

    Agency repository added

---
+++ src/main/java/ir/melkban24/repository/AgencyRepository.java
@@ -0,0 +1,11 @@
+package ir.melkban24.repository;
+
+import ir.melkban24.model.Agency;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by mehdi on 4/13/17.
+ */
+public interface AgencyRepository extends JpaRepository<Agency, String> {
+
+}
