7c462155cb018146ae6676b73f20c642b3daad55
Mehdi <mahdilacki@gmail.com>
2017-04-16 08:38:14 +0430

    Agency creation implemented partly

--- src/main/java/ir/melkban24/controller/AgencyController.java
+++ src/main/java/ir/melkban24/controller/AgencyController.java
@@ -3,10 +3,14 @@ package ir.melkban24.controller;
 import ir.melkban24.model.Agency;
 import ir.melkban24.service.AgencyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,5 +23,19 @@ public class AgencyController {
     @RequestMapping(value = "/agency/list",method = RequestMethod.GET)
     public List<Agency> agencyList() {
         return agencyService.findAll();
+    }
+    @RequestMapping(value = "/agency/create", method = RequestMethod.POST, consumes = "multipart/form-data")
+    public ResponseEntity<List<String>> createAgency(
+            @RequestParam(value = "username", required = true) String username,
+            @RequestParam(value = "pic1", required = true)MultipartFile pic1File,
+            MultipartHttpServletRequest request, ModelAndView modelAndView) {
+        List<String> requestKeys=new ArrayList<String>();
+        List<String> originalFileName=new ArrayList<String>();
+
+        request.getFileNames().forEachRemaining(requestKeys::add);
+        for(String multipartFile:requestKeys) {
+            originalFileName.add(request.getFile(multipartFile).getOriginalFilename());
+        }
+        return new ResponseEntity<List<String>>(originalFileName, HttpStatus.CREATED);
     }
 }
