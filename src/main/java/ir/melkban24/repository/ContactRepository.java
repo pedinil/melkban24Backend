3d1fdf54a950e2256b2103ddf5706a0ad316eeb0
mehdi <mahdilacki@gmail.com>
2017-07-14 13:52:34 +0430

    findAllByAgent_Id added

--- src/main/java/ir/melkban24/repository/ContactRepository.java
+++ src/main/java/ir/melkban24/repository/ContactRepository.java
@@ -1,6 +1,8 @@
 package ir.melkban24.repository;
 
 import ir.melkban24.model.Contact;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -8,5 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by mehdi on 5/24/17.
  */
 public interface ContactRepository extends JpaRepository<Contact,Long>{
-
+    Page<Contact> findAllByAgent_Id(Long id, Pageable pageable);
 }
